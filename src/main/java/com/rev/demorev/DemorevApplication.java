4a81101c5fd41c468f3c7244903c03eb2f49a5e7
Itstep <seleznev_k@itstep.org>
2019-07-18 19:40:53 +0300

    Установлен Spring Framework

---
+++ src/main/java/com/rev/demorev/DemorevApplication.java
@@ -0,0 +1,13 @@
+package com.rev.demorev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemorevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemorevApplication.class, args);
+	}
+
+}
